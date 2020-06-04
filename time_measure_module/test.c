49f04027a6538f1e80b38ce0e2415e4adc1590ad
Mateusz Chrzanowski <59821589+chrzanowskimateusz@users.noreply.github.com>
2020-06-04 20:50:34 +0200

    Dokładny pomiar czasu jako kernel module

---
+++ time_measure_module/test.c
@@ -0,0 +1,48 @@
+//zbiorę do kupy wszystkie polecenia konsolowe i wrzucę na discorda ze screenami
+//i kompilacją/testowaniem
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/hardirq.h>
+#include <linux/preempt.h>
+#include <linux/sched.h>
+
+#include "clock_start.c"
+#include "clock_stop.c"
+
+
+//najlepiej do tego modułu zaincludować load i store, bo nie znam się na tyle dobrze
+//na kernel modules, zeby tamto przebudowywać
+
+unsigned long flags;
+uint64_t start, end, wynik;
+
+static int __init test_start(void){
+
+  int i;
+
+  //pętle tylko testowo sobie zrobiłem, edytujcie wedle uznania parametry
+  for(i=0;i<10;i++){
+
+    preempt_disable();          // te linijki służą do
+    raw_local_irq_save(flags);  //przejęcia pełnej kontrolii nad CPU
+
+    start = clock_start();
+    printk("testtesttest %d\n", 10);  // <- tutaj samego loada do pomiaru, najlepiej jako osobną funkcję
+    end = clock_stop();
+
+    raw_local_irq_restore(flags);
+    preempt_enable();             //wznowienie normalnej pracy
+
+    wynik = (end-start);
+    printk("\n\nWyniki: \nstart = %lld \nend = %lld\nwynik = %lld\n", start, end, wynik);
+  }
+
+return 0;
+}
+
+static void __exit hello_end(void) { printk(KERN_INFO "Exit.\n"); }
+
+module_init(test_start);
+module_exit(hello_end);
