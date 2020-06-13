2df8cbe055cee81f4fc2e5d399c6c5f3d65c485a
Maciej Mączyński <61564189+Wakir1999@users.noreply.github.com>
2020-06-13 12:39:46 +0200

    Update test.c
    Dodanie pojedyńczej funkcji load i store. Przygotowanie finalnej wersji programu, gotowej do testowania.

--- time_measure_module/test.c
+++ time_measure_module/test.c
@@ -7,6 +7,8 @@
 #include <linux/hardirq.h>
 #include <linux/preempt.h>
 #include <linux/sched.h>
+#include <mintomic/mintomic.h>
+
 
 #include "clock_start.c"
 #include "clock_stop.c"
@@ -17,7 +19,19 @@
 //na kernel modules, zeby tamto przebudowywać
 
 unsigned long flags;
-uint64_t start, end, wynik;
+uint64_t start1, end1, wynik1; //wyniki pierwszych pomiarow
+uint64_t start2, end2, wynik2; //wyniki drugich pomiarow
+uint64_t roznica; //roznica pomiedzy drugim wynikiem a pierwszym
+mint_atomic64_t sharedValue = { 0 };
+
+void storeValue(void)
+{
+    mint_store_64_relaxed(&sharedValue, 0x100000014);
+}
+uint64_t loadValue(void)
+{
+    return mint_load_64_relaxed(&sharedValue);
+}
 
 static int __init test_start(void){
 
@@ -28,18 +42,26 @@ static int __init test_start(void){
 
     preempt_disable();          // te linijki służą do
     raw_local_irq_save(flags);  //przejęcia pełnej kontrolii nad CPU
+    
+    storeValue();
+    start1=clock_start();
+    loadValue();
+    end1 = clock_stop();
+    
     int address = makeSpace();
     for(int p = 64; p < number_of_pages; p++) //powinno przesunac o jedna strone do przodu po zapisaniu całego okienka
         store(address, p);
-    start = clock_start();
-    printk("testtesttest %d\n", 10);  // <- tutaj samego loada do pomiaru, najlepiej jako osobną funkcję
-    end = clock_stop();
+    start2 = clock_start();
+    loadValue();  // <- tutaj samego loada do pomiaru, najlepiej jako osobną funkcję
+    end2 = clock_stop();
 
     raw_local_irq_restore(flags);
     preempt_enable();             //wznowienie normalnej pracy
 
-    wynik = (end-start);
-    printk("\n\nWyniki: \nstart = %lld \nend = %lld\nwynik = %lld\n", start, end, wynik);
+    wynik1 = (end1-start1);
+    printk("\n\nWynik 1 pomiaru: \nstart = %lld \nend = %lld\nwynik = %lld\n", start1, end1, wynik1);
+    wynik2 = (end2-start2);
+    printk("\n\nWyniki 2 pomiaru: \nstart = %lld \nend = %lld\nwynik = %lld\n", start2, end2, wynik2);
   }
 
 return 0;
